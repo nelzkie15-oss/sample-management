f5088effc18b90d6c21093fd534fb29501a42aba
nelzkie15-oss <67332045+nelzkie15-oss@users.noreply.github.com>
2023-09-23 17:26:47 +0800

    commit
    login and connect

--- config/connection.php
+++ config/connection.php
@@ -1,9 +1,14 @@
 <?php
 
-     define('DB_HOST1', 'localhost');
-	 define('DB_USER1', 'root');
-	 define('DB_PASS1', '');
-	 define('DB_NAME1', 'sample_management');
+     // define('DB_HOST1', 'localhost');
+	//  define('DB_USER1', 'root');
+	//  define('DB_PASS1', '');
+	//  define('DB_NAME1', 'sample_management');
+
+      define('DB_HOST1', 'localhost');
+	 define('DB_USER1', 'u130149558_roots');
+	 define('DB_PASS1', 'HC|Xu]Ns;z7');
+	 define('DB_NAME1', 'u130149558_samplemgnt_db');
 
 
  ?>
